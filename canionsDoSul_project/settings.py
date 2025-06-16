768416e8e3f17232a5279495783a971a3d7ecae5
Vinicius Bender <viniciusbender01@outlook.com>
2025-06-16 06:18:12 +0200

    reorg. estrutura

--- canionsDoSul_project/settings.py
+++ canionsDoSul_project/settings.py
@@ -148,3 +148,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configurações do WhiteNoise para arquivos estáticos
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Configuração adicional para servir arquivos estáticos
+WHITENOISE_USE_FINDERS = True
+WHITENOISE_AUTOREFRESH = True
