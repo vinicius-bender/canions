44975eddd6092721cb0e0e682b519f00bc86f13c
vinicius-bender <viniciusbender01@outlook.com>
2025-06-19 17:21:41 -0300

    tamanho imgs/vid tela approval

--- canionsDoSul_project/settings.py
+++ canionsDoSul_project/settings.py
@@ -77,8 +77,8 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT'),
+        'HOST': os.getenv('POSTGRES_HOST', 'db'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
