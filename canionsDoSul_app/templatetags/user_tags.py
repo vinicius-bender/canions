481ea20976282cfe89554e264f2e7f4b91603b98
vinicius-bender <viniciusbender01@outlook.com>
2025-07-01 12:03:19 -0300

    ajuste tags

--- canionsDoSul_app/templatetags/user_tags.py
+++ canionsDoSul_app/templatetags/user_tags.py
@@ -19,12 +19,24 @@ def not_in(value, args):
 
 @register.simple_tag
 def is_specialist_or_scientist(user):
-    return user.role in ['specialist', 'scientist']
+    return user.is_authenticated and user.role in ['specialist', 'scientist']
 
 @register.simple_tag
 def is_specialist_or_scientist_admin(user):
-    return user.role in ['specialist', 'scientist', 'admin']
+    return user.is_authenticated and user.role in ['specialist', 'scientist', 'admin']
 
 @register.simple_tag
 def is_admin(user):
     return user.is_authenticated and user.role == "admin"
+
+# @register.simple_tag
+# def is_specialist_or_scientist(user):
+#     return user.role in ['specialist', 'scientist']
+
+# @register.simple_tag
+# def is_specialist_or_scientist_admin(user):
+#     return user.role in ['specialist', 'scientist', 'admin']
+
+# @register.simple_tag
+# def is_admin(user):
+#     return user.is_authenticated and user.role == "admin"
